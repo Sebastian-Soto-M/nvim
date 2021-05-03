98f7ff7c3053bea4b4cac025977dc3ccc50bd535
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-05-02 22:37:19 -0600

    lspkind

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,7 @@ require('plugins._packer')
 
 require('plugins._completion')
 require('plugins._formatter')
-require('plugins._telescope')
+require('plugins._lspkind')
 require('plugins._lspsaga')
+require('plugins._telescope')
 require('plugins.git')
