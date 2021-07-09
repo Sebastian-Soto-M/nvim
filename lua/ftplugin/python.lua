38f2972c2fa625aca43af6e147172110613b79e1
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-07-08 23:08:46 -0600

    removed aucmd to sort imports python

--- lua/ftplugin/python.lua
+++ lua/ftplugin/python.lua
@@ -6,7 +6,3 @@ U.apply_local_indentation(4)
 D.initialize_mappings('pymake')
 D.map_run_file('python')
 
--- coc-pyright
-vim.api
-    .nvim_exec('autocmd BufWritePre *.py CocCommand python.sortImports', true)
-
