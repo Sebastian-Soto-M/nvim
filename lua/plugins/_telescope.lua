83ee264108228c9ef6c72d6f70d9849977ad4541
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2021-07-09 22:34:55 -0600

    updated telescope plugin config

--- lua/plugins/_telescope.lua
+++ lua/plugins/_telescope.lua
@@ -3,25 +3,38 @@ local previewers = require("telescope.previewers")
 
 require('telescope').setup({
     defaults = {
-        file_previewer = previewers.vim_buffer_cat.new,
-        grep_previewer = previewers.vim_buffer_vimgrep.new,
-        qflist_previewer = previewers.vim_buffer_qflist.new,
-        scroll_strategy = "cycle",
-        selection_strategy = "reset",
-        layout_strategy = "flex",
-        borderchars = {"─", "│", "─", "│", "┌", "┐", "┘", "└"},
-        layout_defaults = {
-            horizontal = {
-                width_padding = 0.1,
-                height_padding = 0.1,
-                preview_width = 0.6
-            },
-            vertical = {
-                width_padding = 0.05,
-                height_padding = 1,
-                preview_height = 0.5
-            }
+        vimgrep_arguments = {
+            'rg', '--color=never', '--no-heading', '--with-filename',
+            '--line-number', '--column', '--smart-case'
         },
+        scroll_strategy = "cycle",
+        prompt_prefix = "> ",
+        selection_caret = "> ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "descending",
+        layout_strategy = "flex",
+        layout_config = {
+            horizontal = {mirror = false},
+            vertical = {mirror = false}
+        },
+        file_sorter = require'telescope.sorters'.get_fuzzy_file,
+        file_ignore_patterns = {},
+        generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+        winblend = 0,
+        border = {},
+        borderchars = {"─", "│", "─", "│", "┌", "┐", "┘", "└"},
+        color_devicons = true,
+        use_less = true,
+        path_display = {},
+        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
         mappings = {
             i = {
                 ["<C-j>"] = actions.move_selection_next,
