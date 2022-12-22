9cee83d4ec29ce65a0f9ad22b0d144d6a07bb912
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2022-12-21 19:45:23 -0600

    prime's lsp setup

--- after/plugin/packer.lua
+++ after/plugin/packer.lua
@@ -55,24 +55,33 @@ require('packer').startup(function(use)
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
 
-  -- LSP Configuration & Plugins
-  -------------------------------------
-  use { 
-    'neovim/nvim-lspconfig',
-    requires = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+		  -- Snippets
+		  {
+        'L3MON4D3/LuaSnip',
+        'rafamadriz/friendly-snippets',
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip'
+      },
       -- Useful status updates for LSP
       use {'j-hui/fidget.nvim', config = function() require('fidget').setup() end},
       -- Additional lua configuration, makes nvim stuff amazing
       use {'folke/neodev.nvim', config = function() require('neodev').setup() end},
-    },
-  }
-  -- Autocompletion
-  use { 
-    'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+	  }
   }
 
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
